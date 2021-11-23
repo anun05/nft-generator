685c1c500487ffeeac40d5d9c1f5f5c86aa099ac
jdesgarennes <johndesgarennes@gmail.com>
2021-11-22 20:32:48 -0800

    chamnged case

---
+++ models/Nft.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection.js');
+//
+class Nft extends Model {}
+
+    Nft.init(
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          nft_name:{
+              type: DataTypes.STRING,
+              allowNull: false,
+              validate: {
+                max: 23, 
+              },
+          },
+          public_key: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [42],
+              },
+          },
+          description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate:{
+              max: 200,
+            }
+          }
+
+          },
+          {
+            hooks: {
+              beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+              },
+              beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(
+                  updatedUserData.password,
+                  10
+                );
+
+                return updatedUserData;
+              },
+            },
+            sequelize,
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: "user",
+          }
+          
+          
+    );
+
+    module.exports = Nft;
