8083e487a03fbf4d2e8bb0764cdd297319ae2c29
Israel M. Rios <israelm.riosjr@gmail.com>
2021-11-27 18:01:15 -0800

    fixed create new nft bug

--- controllers/api/nftRoutes.js
+++ controllers/api/nftRoutes.js
@@ -1,20 +1,39 @@
 const router = require("express").Router();
-const { Nft } = require("../../models");
-const withAuth = require('../../utils/auth');
-require('dotenv').config();
+const { Nft, User } = require("../../models");
+const withAuth = require("../../utils/auth");
+require("dotenv").config();
 
-router.post('/', withAuth, async (req, res) => {
-    try {
-      const newNft = await Post.create({
-        ...req.body,
-        user_id: req.session.user_id,
-        public_key: process.env.PUBLIC_KEY,
-      });
-  
-      res.status(200).json(newNft);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const nftData = await Nft.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const nfts = nftData.map((nft) => nft.get({ plain: true }));
+
+    res.json(nfts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newNft = await Nft.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      public_key: process.env.PUBLIC_KEY,
+    });
+
+    res.status(200).json(newNft);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/nft.js
+++ public/js/nft.js
@@ -1,23 +1,22 @@
 const createBtn = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#nameNFT").value.trim();
-  const url = document.querySelector("#imageUrl").value.trim();
+  const nft_name = document.querySelector("#nameNFT").value.trim();
+  const image_url = document.querySelector("#imageUrl").value.trim();
   const description = document.querySelector("#descriptionNFT").value.trim();
 
-  if (name && url && description) {
-    const response = await fetch(`/api/nfts`, {
-      method: "POST",
-      body: JSON.stringify({ name, url, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+  if (nft_name && image_url && description) {
+    const response = await fetch('/api/nfts', {
+      method: 'POST',
+      body: JSON.stringify({ nft_name, image_url, description }),
+      headers: { "Content-Type": "application/json" },
     });
 
+    console.log(response);
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/nftpage");
     } else {
-      alert("Failed to create project");
+      alert("Failed to create NFT");
     }
   }
 };
