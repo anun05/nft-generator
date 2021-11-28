a8e94f33297db1e41ea8735bd335548f94643e1b
jdesgarennes <johndesgarennes@gmail.com>
2021-11-28 12:25:31 -0800

    cleanup

--- utils/pinata.js
+++ utils/pinata.js
@@ -4,33 +4,13 @@ const mintNFT = require('../public/js/mint');
 const pinata = pinataSDK('cecdbdbb43a28ca3383b', '207a0968305b01775e4cd5a6443cddba0f0e3a8251e005b1ab12dc98c413b34e');
 
 
-// pinata.testAuthentication().then((result) => {
-//     //handle successful authentication here
-//     console.log(result);
-// }).catch((err) => {
-//     //handle error here
-//     console.log(err);
-// });
-
-
 const pinme = (filePath) =>{
 
 
 
 const fs = require('fs');
 const readableStreamForFile = fs.createReadStream(filePath);
-// const options = {
-//     pinataMetadata: {
-//         name: MyCustomName,
-//         keyvalues: {
-//             customKey: 'customValue',
-//             customKey2: 'customValue2'
-//         }
-//     },
-//     pinataOptions: {
-//         cidVersion: 0
-//     }
-// };
+
 pinata.pinFileToIPFS(readableStreamForFile, ).then((result) => {
     //handle results here
     console.log(result.IpfsHash);
